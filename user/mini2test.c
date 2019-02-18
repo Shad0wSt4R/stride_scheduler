a463fa7222a5f66ec6dc2179c2026f31f19b4545
NicoleBruce <ZTheCrazed@Gmail.com>
2019-02-18 17:11:04 -0500

    Fixed some errors

--- user/mini2test.c
+++ user/mini2test.c
@@ -11,14 +11,14 @@ mini2test(void){
    int i;
    for(i =0;i<N;i++){
      p.inuse[i] = 0;
-     p.tickets = 10;
-     p.pid = -1;
-     p.ticks = 0;
+     p.tickets[i] = 10;
+     p.pid[i] = -1;
+     p.ticks[i] = 0;
    }
    int info = getpinfo(&p);
    printf(2, "Here is a list");
    for(i=0;i<N;i++){
-     print(2, "Process:: inuse: %d, tickets: %d, pid: %d, ticks: %d", p.inuse[i], p.tickets[i], p.pid[i], p.ticks[i]);
+     printf(2, "Process:: inuse: %d, tickets: %d, pid: %d, ticks: %d", p.inuse[i], p.tickets[i], p.pid[i], p.ticks[i]);
    }
    return info;
 }
