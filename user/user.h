7a68cd93f92bdefb3692dbb30c65bb9ef674511f
NicoleBruce <ZTheCrazed@Gmail.com>
2019-02-18 15:09:36 -0500

    Fixed error in parameter declarations

--- user/user.h
+++ user/user.h
@@ -26,7 +26,7 @@ int getpid(void);
 char* sbrk(int);
 int sleep(int);
 int uptime(void);
-int settickets(int tickets); //ADDED SET TICKETS HERE
+int settickets(int); //ADDED SET TICKETS HERE
 int getpinfo(struct pstat*); //ADDED GET P INFO HERE
 
 // user library functions (ulib.c)
