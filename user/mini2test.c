e21286def04f5b3dfabab6c711bfc2e7092af736
NicoleBruce <ZTheCrazed@Gmail.com>
2019-02-25 12:29:17 -0500

    Made adjustments to printing

--- user/mini2test.c
+++ user/mini2test.c
@@ -11,14 +11,14 @@ mini2test(void){
    int i;
    for(i =0;i<N;i++){
      p.inuse[i] = 0;
-     p.tickets[i] = 10;
+     p.tickets[i] = 0;
      p.pid[i] = -1;
      p.ticks[i] = 0;
    }
    int info = getpinfo(&p);
-   printf(2, "Here is a list");
+   printf(2, "Here is a list of every process\n");
    for(i=0;i<N;i++){
-     printf(2, "Process:: inuse: %d, tickets: %d, pid: %d, ticks: %d\n", p.inuse[i], p.tickets[i], p.pid[i], p.ticks[i]);
+     printf(2, "Process:: pid: %d inuse: %d, tickets: %d, passvalue: %d, stride: %d, ticks: %d\n", p.pid[i], p.inuse[i], p.tickets[i], p.passvalue[i], p.stride[i], p.ticks[i]);
    }
    return info;
 }
